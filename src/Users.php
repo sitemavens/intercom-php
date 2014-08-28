5dd1d2e641d50705b0b9a2610392100b6fce4eb8
mustela <ejankowski@gmail.com>
2014-08-28 10:58:50 -0300

    Composer added

---
+++ src/Users.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace IntercomPhp;
+
+class Users{
+	
+	public function addUser(){
+		
+		$response = \GuzzleHttp\post('https://api.intercom.io/users',[
+			'header'=>'Authorization: Basic pi3243fa:da39a3ee5e6b4b0d3255bfef95601890afd80709'
+		]);
+		
+		var_dump($response);
+		
+	}
+	
+	
+}
